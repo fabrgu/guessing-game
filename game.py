0c3fbd3a9fb0eaf70416dc48b0658052018fbeac
Your Name <you@example.com>
2020-01-29 12:00:46 -0800

    take care of invalid input

--- game.py
+++ game.py
@@ -2,6 +2,16 @@
 
 import random
 
+
+def get_valid_user_input(message):
+    guess = None
+    try:
+        guess = int(input(message))
+    except ValueError:
+        print("Not a number, silly!")
+    return guess
+
+
 print("Howdy, what's your name?")
 user_name = input("(type in your name) ")
 print(f"{user_name} I'm thinking of a number between 1 and 100.")
@@ -9,11 +19,29 @@ print("Try to guess my number.")
 
 rand_num = random.randrange(101)
 
-
-guess = int(input("Your guess? "))
+guess = get_valid_user_input("Your guess? ")
 guess_count = 1
 
+while guess is None:
+    guess = get_valid_user_input("Please enter a real number: ")
+    guess_count += 1
+
+while guess < 1 or guess > 100:
+    print("That's not between 1 and 100 silly!")
+    guess = get_valid_user_input(
+        f"Please enter a valid number between 1 and 100: ")
+    guess_count += 1
+
+
 while True:
+    if guess is None:
+        guess = get_valid_user_input("Please enter a real number: ")
+
+    if guess < 1 or guess > 100:
+        print("That's not between 1 and 100 silly!")
+        guess = get_valid_user_input(
+            f"Please enter a valid number between 1"
+            f"and 100: ")
     if guess == rand_num:
         print(
             f"Well done, {user_name}!"
@@ -26,5 +54,15 @@ while True:
     elif guess > rand_num:
         print("Your guess is too high, try again.")
 
-    guess = int(input("Your guess? "))
+    guess = get_valid_user_input("Your guess? ")
     guess_count += 1
+    while guess is None:
+        guess = get_valid_user_input("Please enter a real number: ")
+        guess_count += 1
+
+    while guess < 1 or guess > 100:
+        print("That's not between 1 and 100 silly!")
+        guess = get_valid_user_input(
+            f"Please enter a valid number "
+            f"between 1 and 100: ")
+        guess_count += 1
